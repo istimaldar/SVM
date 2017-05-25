105f1535cae23b8d02dcb3168ac5dc4095967954
istimaldar <istimaldar@gmail.com>
2017-05-25 14:27:36 +0300

    Cramer method

--- SVM.py
+++ SVM.py
@@ -17,6 +17,54 @@ class SVM:
         return sum(vector) ** 0.5
 
     @staticmethod
+    def minor(X, i, j):
+        result = []
+        for i1, x in enumerate(X):
+            if i1 == i:
+                continue
+            temp = []
+            for j1, y in enumerate(x):
+                if j1 == j:
+                    continue
+                temp.append(y)
+            result.append(temp)
+        return result
+
+    @staticmethod
+    def determinant(X):
+        assert X, list
+        size = len(X[0])
+        for x in X:
+            assert x, list
+            if len(x) != size:
+                raise ValueError("Wrong matrix")
+        if len(X) == 1:
+            return X[0][0]
+        result = 0
+        for i in range(len(X)):
+            result += ((-1) ** i) * X[0][i] * SVM.determinant(SVM.minor(X, 0, i))
+        return result
+
+    @staticmethod
+    def cramer_matrix(X, j):
+        assert X, list
+        for x in X:
+            assert x, list
+            if len(x) != len(X) + 1:
+                raise ValueError("Wrong matrix size")
+        result = []
+        for i1 in range(len(X)):
+            temp = []
+            for j1 in range(len(X)):
+                temp.append(X[i1][j1] if j1 != j else X[i1][len(X)])
+            result.append(temp)
+        return result
+
+    @staticmethod
+    def cramer_main_matrix(X):
+        return [x[:-1] for x in X]
+
+    @staticmethod
     def linear_kernel(X, Y, c=0):
         return SVM.multiply_vector(X, Y) + c
 
@@ -32,10 +80,15 @@ class SVM:
     kernel_types = {"linear": linear_kernel, "polynomial": polynomial_kernel, "gaussian": gaussian_kernel}
 
     def __init__(self, kernel_type, X, Y):
-        pass
-
-
+        assert X, list
+        for x in X:
+            assert x, list
+        assert Y, list
+        for y in Y:
+            assert y, list
+        if len(X) != len(Y):
+            raise ValueError("X and Y should be the same size")
 
 
 if __name__ == "__main__":
-    print(SVM.multiply_vector([-1, -1], [-1, -1]))
+    print(SVM.cramer_matrix([[1, 2, -3, 0], [3, 2, -1, 0], [4, 2, -1, 1]], 0))

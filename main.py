38100bd8baef9995b3324c3a89cf610e8677510f
istimaldar <istimaldar@gmail.com>
2017-06-12 14:46:09 +0300

    Try right data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from os.path import isfile, join
 
 import pickle
@@ -9,13 +10,22 @@ from kernels import GAUSSIAN
 if __name__ == '__main__':
     machine = ovr.OVR()
     X = []
+    Y = []
     t = 0
     for name in [f for f in os.listdir('train_data/') if isfile(join('train_data/', f))]:
-        if t == 60:
+        if t == 55:
             break
         t += 1
+        with open('diagnoses/{}'.format(name), 'w') as f:
+            f.write(str(int(random.random() * 40)))
         with open('train_data/{}'.format(name), 'rb') as f:
             X.append(pickle.load(f))
-    Y = [i for i in range(60)]
-    machine.train(60, GAUSSIAN, [2], X, Y, 2)
-    print(machine.classify(X[2]))
+        with open('diagnoses/{}'.format(name), 'r') as f:
+            data = f.readline().rstrip()
+            Y.append(int(data))
+    machine.train(55, GAUSSIAN, [2], X, Y, 4)
+    wrong = 0
+    for x in range(55):
+        if machine.classify(X[x]) != Y[x]:
+            wrong += 1
+    print(wrong)

86038af95f4f9a5b21983c4eb14779608a38d61b
istimaldar <istimaldar@gmail.com>
2017-06-12 14:59:49 +0300

    Random

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ import ovr
 from kernels import GAUSSIAN
 
 if __name__ == '__main__':
-    machine = ovr.OVR()
-    X = []
-    Y = []
-    t = 0
-    for name in [f for f in os.listdir('train_data/') if isfile(join('train_data/', f))]:
-        if t == 55:
-            break
-        t += 1
-        with open('diagnoses/{}'.format(name), 'w') as f:
-            f.write(str(int(random.random() * 40)))
-        with open('train_data/{}'.format(name), 'rb') as f:
-            X.append(pickle.load(f))
-        with open('diagnoses/{}'.format(name), 'r') as f:
-            data = f.readline().rstrip()
-            Y.append(int(data))
-    machine.train(55, GAUSSIAN, [2], X, Y, 4)
-    wrong = 0
-    for x in range(55):
-        if machine.classify(X[x]) != Y[x]:
-            wrong += 1
-    print(wrong)
+    wrong = 72
+    while (wrong / 72) > 0.05:
+        machine = ovr.OVR()
+        X = []
+        Y = []
+        t = 0
+        for name in [f for f in os.listdir('train_data/') if isfile(join('train_data/', f))]:
+            if t == 55:
+                break
+            t += 1
+            with open('diagnoses/{}'.format(name), 'w') as f:
+                f.write(str(int(random.random() * 40)))
+            with open('train_data/{}'.format(name), 'rb') as f:
+                X.append(pickle.load(f))
+            with open('diagnoses/{}'.format(name), 'r') as f:
+                data = f.readline().rstrip()
+                Y.append(int(data))
+        sigma = random.random() * 10
+        C = random.random() * 300
+        machine.train(55, GAUSSIAN, [sigma], X, Y, C)
+        wrong = 0
+        for x in range(55):
+            if machine.classify(X[x]) != Y[x]:
+                wrong += 1
+        print(wrong / 72, sigma, C)
